28327dc78383ab8a718b05c7bd62842820200caa
william rodrigues <38387964+williamrx777@users.noreply.github.com>
2025-04-05 20:44:51 -0300

    alter:url in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="image processing",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url=""
+    url="https://github.com/williamrx777/package-template-master"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.6',
